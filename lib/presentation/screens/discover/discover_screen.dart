135eb9246452e9a1d59e6605a15108075a5da5cc
Fabian Guerrero <frguerrerogo@gmail.com>
2023-10-19 09:12:09 -0500

    First commit

---
+++ lib/presentation/screens/discover/discover_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:toktik_app_3/presentation/providers/discover_provider.dart';
+import 'package:toktik_app_3/presentation/widgets/shared/video_scrollable_view.dart';
+
+class DiscoverScreen extends StatelessWidget {
+  const DiscoverScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final discoverProvider = context.watch<DiscoverProvider>();
+
+    return Scaffold(body: discoverProvider.initialLoading ? const Center(child: CircularProgressIndicator(strokeWidth: 2)) : VideoScrollableView(videos: discoverProvider.videos));
+  }
+}
